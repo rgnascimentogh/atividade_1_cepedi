399bb3c387d5073e9dbca48eefd137488a06e56d
Ricardo Nascimento <rgnascimentoms@hotmail.com>
2024-12-23 20:26:31 -0300

    criando uma função de conversão para diferentes uinidades de potência

---
+++ conv_unid.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+float pot(){
+    system("chcp 65001>null");
+    int uni;
+    printf("\nEscolha a unidade de entrada: ");
+    printf("\n1-Watts\n2-kWatts\n3-Cavalo-vapor(cv)\n");
+    scanf("%f",&uni);
+    switch (uni)
+    {
+    case 1:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
