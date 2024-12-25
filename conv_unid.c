17fb15261c70005ee3f60cf3f742e93a0eab345b
Ricardo Nascimento <rgnascimentoms@hotmail.com>
2024-12-25 20:11:12 -0300

    programa finaizado, funcionando perfeitamente!

--- conv_unid.c
+++ conv_unid.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include"conv.h" //puxa as macros do arquivo conv.h
 
 float potencia(void){
     system("chcp 65001>null");
     int uni;
-    float watts, kwatts, cv, pot;
+    float pot;
     printf("\nEscolha a unidade de entrada: ");
     printf("\n1-Watts\n2-kWatts\n3-Cavalo-vapor(cv)\n");
     scanf("%d",&uni);
@@ -14,24 +15,21 @@ float potencia(void){
     switch (uni)
     {
     case 1://define watts em kwatts e cv.
-        kwatts = pot*1/1000;
-        cv = pot*1/735.5;
-        printf("\n%.2f kWatts",kwatts);
-        printf("\n%.2f cv",cv);
+        
+        printf("\n%.2f Watts = %.2f kWatts",pot,WkW(pot));
+        printf("\n%.2f Watts = %.2f CV",pot,WCV(pot));
         break;
 
     case 2://define kWatts em Watts e CV.
-        watts = pot*1000;
-        cv = pot/0.7355;
-        printf("\n%.2f Watts",watts);
-        printf("\n%.2f cv",cv);
+        
+        printf("\n%.2f kWatts = %.2f Watts",pot,kWW(pot));
+        printf("\n%.2f kWatts = %.2f CV",pot,kWCV(pot));
         break;
     
     case 3://define cv em Watts e kWatts.
-        watts = pot*735.5;
-        kwatts = watts/1000;
-        printf("\n%.2f Watts",watts);
-        printf("\n%.2f cv",kwatts);
+        
+        printf("\n%.2f CV = %.2f Watts",pot,CVW(pot));
+        printf("\n%.2f CV = %.2f kWatts",pot,CVkW(pot));
         break;
     
     default:
@@ -39,10 +37,4 @@ float potencia(void){
     }
 }
 
-void main(void){
 
-    potencia();
-
-
-    return 0;
-}
